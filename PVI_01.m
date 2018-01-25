85dd79e0543069e325d45fec82d9a7505b855616
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2018-01-25 20:25:58 +0100

    Ejercicio 02 - Quiza falta dividir el plot de error en 2

--- PVI_01.m
+++ PVI_01.m
@@ -1,3 +1,7 @@
+close all
+clc
+
+
 f = @(t,y) 10*(2 - y);
 tspan = [0 3];
 h1 = 0.3;
@@ -24,3 +28,38 @@ figure
 plot(t3, y3, t3, sol(t3))
 legend('aprox', 'exacta');
 
+
+h4 = 0.025;
+
+[t4, y4] = beuler(f, tspan, y0, (tspan(2) - tspan(1))/h4);
+figure
+plot(t4, y4, t4, sol(t4))
+legend('aprox', 'exacta');
+[t4, y4](rows(t4),:)
+
+
+[t5, y5] = cranknic(f, tspan, y0, (tspan(2) - tspan(1))/h4);
+figure
+plot(t5, y5, t5, sol(t5))
+legend('aprox', 'exacta');
+[t5', y5](rows(t5'),:)
+
+valorAnalitico = sol(0.3)
+valorBeuler = y4(lookup(t4, 0.3))
+valorCN = y5(lookup(t5, 0.3))
+
+errorBeuler = abs(valorAnalitico - valorBeuler)
+errorCN = abs(valorCN - valorBeuler)
+
+if errorBeuler < errorCN
+  disp("Beuler mas precios")
+else
+  disp("CN mas preciso")
+end
+
+errores = abs(y4 .- y5);
+
+figure
+plot(t5, errores)
+title("Error")
+legend("Error beuler/cranknic")
